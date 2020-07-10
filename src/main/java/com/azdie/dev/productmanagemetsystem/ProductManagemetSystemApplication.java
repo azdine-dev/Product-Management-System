ab79690c563707298bbbc8c1df6bef1ffcd878b0
Azeddine BENHLIMA <azeddinebenhalima@gmail.com>
2020-07-10 18:11:32 +0100

    first commit

---
+++ src/main/java/com/azdie/dev/productmanagemetsystem/ProductManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.azdie.dev.productmanagemetsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagemetSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagemetSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azdie/dev/productmanagemetsystem/ProductManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azdie.dev.productmanagemetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagemetSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
